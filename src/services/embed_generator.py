95b7135c08d1cd830fc6b18fd9c413a227b89561
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-10-22 13:20:56 -0400

    Rm emojis

--- src/services/embed_generator.py
+++ src/services/embed_generator.py
@@ -31,31 +31,31 @@ class EmbedGenerator:
             if team.family_task[1] < datetime.now():
                 family_reroll_text = "🎲 *You can re-roll*"
             
-            partial_counter_text_mini = "🟡 Requires **1** submission"
-            partial_counter_text_fun = "🟡 Requires **1** submission"
-            partial_counter_text_full = "🟡 Requires **1** submission"
-            partial_counter_text_family = "🟡 Requires **1** submission"
-            partial_counter_text_bucket = "🟡 Requires **1** submission"
+            partial_counter_text_mini = "Requires **1** submission"
+            partial_counter_text_fun = "Requires **1** submission"
+            partial_counter_text_full = "Requires **1** submission"
+            partial_counter_text_family = "Requires **1** submission"
+            partial_counter_text_bucket = "Requires **1** submission"
             
             if int(team.mini_task[0]['CompletionCounter']) > 1:
-                partial_counter_text_mini = f"🟡 Requires **{team.mini_task[0]['CompletionCounter']}** more submissions"
+                partial_counter_text_mini = f"Requires **{team.mini_task[0]['CompletionCounter']}** more submissions"
             if int(team.fun_task[0]['CompletionCounter']) > 1:
-                partial_counter_text_fun = f"🟡 Requires **{team.fun_task[0]['CompletionCounter']}** more submissions"
+                partial_counter_text_fun = f"Requires **{team.fun_task[0]['CompletionCounter']}** more submissions"
             if int(team.full_task[0]['CompletionCounter']) > 1:
-                partial_counter_text_full = f"🟡 Requires **{team.full_task[0]['CompletionCounter']}** more submissions"
+                partial_counter_text_full = f"Requires **{team.full_task[0]['CompletionCounter']}** more submissions"
             if int(team.family_task[0]['CompletionCounter']) > 1:
-                partial_counter_text_family = f"🟡 Requires **{team.family_task[0]['CompletionCounter']}** more submissions"
+                partial_counter_text_family = f"Requires **{team.family_task[0]['CompletionCounter']}** more submissions"
             if team.bucket_task:
                 if int(team.bucket_task[0]['CompletionCounter']) > 1:
                     partial_counter_text_bucket = f"Requires **{team.bucket_task[0]['CompletionCounter']}** submissions"
     
             desc = f"""Submit any of the following drops in https://discord.com/channels/1290136938115891220/1295850896009330698 to get candy!
-                    \n🍬 **Mini-sized candy bar** (+5)\n> {team.mini_task[0]["Description"]} - [wiki]({team.mini_task[0]["WikiUrl"]})\n> {partial_counter_text_mini}\n> {mini_reroll_text}
-                    \n🍭**Fun-sized candy bar** (+30)\n> {team.fun_task[0]["Description"]} - [wiki]({team.fun_task[0]["WikiUrl"]})\n> {partial_counter_text_fun}\n> {fun_reroll_text}
-                    \n🍭**Full-sized candy bar** (+120)\n> {team.full_task[0]["Description"]} - [wiki]({team.full_task[0]["WikiUrl"]})\n> {partial_counter_text_full}\n> {full_reroll_text}
-                    \n🍫**Family-sized candy bar** (+250)\n> {team.family_task[0]["Description"]} - [wiki]({team.family_task[0]["WikiUrl"]})\n> {partial_counter_text_family}\n> {family_reroll_text}"""
+                    \n**Mini-sized candy bar** (+5)\n> {team.mini_task[0]["Description"]} - [wiki]({team.mini_task[0]["WikiUrl"]})\n> {partial_counter_text_mini}\n> {mini_reroll_text}
+                    \n**Fun-sized candy bar** (+30)\n> {team.fun_task[0]["Description"]} - [wiki]({team.fun_task[0]["WikiUrl"]})\n> {partial_counter_text_fun}\n> {fun_reroll_text}
+                    \n**Full-sized candy bar** (+120)\n> {team.full_task[0]["Description"]} - [wiki]({team.full_task[0]["WikiUrl"]})\n> {partial_counter_text_full}\n> {full_reroll_text}
+                    \n**Family-sized candy bar** (+250)\n> {team.family_task[0]["Description"]} - [wiki]({team.family_task[0]["WikiUrl"]})\n> {partial_counter_text_family}\n> {family_reroll_text}"""
             if team.bucket_task:
-                desc += f"""\n\n🪣 **Candy bucket** (+600)\n> {team.bucket_task[0]["Name"]} - [wiki]({team.bucket_task[0]["WikiUrl"]})\n> {partial_counter_text_bucket}\n> {bucket_expire_text}"""
+                desc += f"""\n\n**Candy bucket** (+600)\n> {team.bucket_task[0]["Name"]} - [wiki]({team.bucket_task[0]["WikiUrl"]})\n> {partial_counter_text_bucket}\n> {bucket_expire_text}"""
            
             embed = discord.Embed(title=f"{team.name}",
                         description= str(desc) ,
